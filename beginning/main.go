6d74850f09a4f768e6d6686c806e2ccec0c0c78d
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-11-02 11:17:52 -0300

    Tests added for morestrings.reverserunes

---
+++ beginning/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"beginning/hello/morestrings"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
